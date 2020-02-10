9ebd654c366633a0b806fb09ca0694f5a489a6da
JinyuChata <jinyuzhu@smail.nju.edu.cn>
2020-02-10 13:53:24 +0800

    login in html

--- src/main/java/org/chu/community/controller/AuthorizeController.java
+++ src/main/java/org/chu/community/controller/AuthorizeController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class AuthorizeController {
 
@@ -26,7 +29,9 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request       // 自动放入Request
+    ) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -35,7 +40,15 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            return "redirect:/";
+            // 登录成功，写Cookie与SessionX
+        } else {
+            // 登录失败，重新登录
+            return "redirect:/";
+        }
+
     }
 }
