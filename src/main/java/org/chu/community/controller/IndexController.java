35c7f517eb7fe83fb55fe14dc8a42dd0e937c55c
JinyuChata <jinyuzhu@smail.nju.edu.cn>
2020-02-12 17:05:32 +0800

    navbar

--- src/main/java/org/chu/community/controller/IndexController.java
+++ src/main/java/org/chu/community/controller/IndexController.java
@@ -15,9 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class IndexController {
